a0bb972ae62cc8611ac336da7fb760608f60614b
KimHunJin <ysg01129@naver.com>
2016-09-07 20:33:34 +0900

    making mage is complete

--- Algorithm/src/DFS.java
+++ Algorithm/src/DFS.java
@@ -3,10 +3,10 @@
  */
 public class DFS {
 
-    int range = 10;
+    int range = 5;
 
     int[][] graph = new int[range][range];
-    int[] visit = new int[range];
+    boolean[] visit = new boolean[range]; // if visit them true else then false
 
     public static void main(String[] args) {
 
@@ -16,18 +16,48 @@ public class DFS {
     }
 
     void init() {
-        for(int i=0;i<range;i++) {
-            for(int j=0;j<range;j++) {
-                graph[i][j] = (int)(Math.random()*2);
-            }
-        }
+        graph[0][0] = 1;
+        graph[range-1][range-1] = 1;
+
+        makeGraph();
+
         print();
     }
 
-    void dfs () {
+    /**
+     * this method is making graph
+     * just right and up down because if has left then so many root
+     * start is 0,0
+     * end is range-1, range-1
+     */
+    void makeGraph() {
+        int x =0, y=0;
+        int random;
 
+        while (x!=range-1 || y!=range-1) {
+            random = (int)(Math.random()*4);
+            if(random==0 && x<range-1) {
+                x++;
+            } else if(random==1 && y<range-1) {
+                y++;
+            } else if(random==2 && x>0) {
+                x--;
+            }
+            graph[x][y] = 1;
+        }
     }
 
+    /**
+     * dfs algorithm
+     *
+     */
+    void dfs () {
+        
+    }
+
+    /**
+     * print method
+     */
     void print() {
         for (int[] ints : graph) {
             for (int anInt : ints) {
