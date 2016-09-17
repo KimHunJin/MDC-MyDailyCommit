6d3f6194cedbef0be67d70c3bb4bf24709f34938
KimHunJin <ysg01129@naver.com>
2016-09-17 17:33:22 +0900

    Basic LambdaExpression

--- Lambda/src/LamdaExpression.java
+++ Lambda/src/LamdaExpression.java
@@ -3,8 +3,12 @@
  */
 public class LamdaExpression {
     public static void main(String[] args) {
-        (a,b)->a>b?a:b;
-    }
+	  Runnable lambda = () -> System.out.println(1); // 익명함수
+	  lambda.run();
+
+	  new LamdaExpression().print();
+	
+	}
 
     /**
      * not lambda
@@ -13,4 +17,26 @@ public class LamdaExpression {
     int max(int a, int b) {
         return a > b ? a : b;
     }
+
+	/**
+	 * using lambda
+	 * 객체 생성 유무 판단
+	 */
+	void print() {
+	  Runnable anonClass = new Runnable() {
+		@Override
+		public void run() {
+		  verifyRunnable(this);
+		}
+	  };
+
+	  anonClass.run(); // 객체 생성 true
+
+	  Runnable lambda = () -> verifyRunnable(this);
+	  lambda.run();  // 객체 생성 false
+	}
+
+	private void verifyRunnable(Object obj) {
+	  System.out.println(obj instanceof Runnable);
+	}
 }
