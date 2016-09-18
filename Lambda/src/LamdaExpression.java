d69e1887e32d1c317428d88bc6e1754591e6f727
KimHunJin <ysg01129@naver.com>
2016-09-18 21:18:52 +0900

    dump

--- Lambda/src/LamdaExpression.java
+++ Lambda/src/LamdaExpression.java
@@ -2,15 +2,9 @@
  * Created by HunJin on 2016-09-16.
  */
 public class LamdaExpression {
+
     public static void main(String[] args) {
-        (a,b)->a>b?a:b;
+
     }
 
-    /**
-     * not lambda
-     * @return
-     */
-    int max(int a, int b) {
-        return a > b ? a : b;
-    }
 }
