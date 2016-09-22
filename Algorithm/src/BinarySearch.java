496736a72d8a262af508a88b9e48be350617547c
KimHunJin <ysg01129@naver.com>
2016-09-22 23:18:30 +0900

    BinarySearch add Comment

--- Algorithm/src/BinarySearch.java
+++ Algorithm/src/BinarySearch.java
@@ -15,14 +15,21 @@ public class BinarySearch {
         System.out.println(binarySearch((int) (Math.random() * 100) + 1));
     }
 
+    /**
+     * 100개의 배열에 랜덤으로 수를 저장한다.
+     */
     void init() {
         for(int i=0;i<number.length;i++) {
             number[i] = (int)(Math.random()*100)+1;
         }
     }
 
-    int tmp;
+
+    /**
+     * 수들을 정렬한다.
+     */
     void sort() {
+        int tmp;
         for(int i=0;i<number.length;i++) {
             for(int j=0;j<number.length;j++) {
                 if(number[i]<number[j]) {
@@ -34,18 +41,28 @@ public class BinarySearch {
         }
     }
 
+    /**
+     * 수의 중간을 기점으로 반복하여 검색
+     * 수를 찾는데 걸리는 횟수와 찾은 수를 출력.
+     * @param findNum
+     * @return 찾은 수
+     */
     int binarySearch(int findNum) {
-        int fIndex = 0;
-        int lIndex = number.length;
-        int count = 0;
-        int mid;
+        int fIndex = 0; // 초기값은 0
+        int lIndex = number.length; // 초기 값은 배열의 크기
+        int count = 0; // 횟수
+        int mid; // 중앙값
+
+        // fIndex와 lIndex가 같거나 값을 찾을때까지 반복
         while (fIndex<=lIndex) {
             mid = (fIndex + lIndex) / 2;
+            // 찾았으면 리턴.
             if(findNum == number[mid]) {
                 System.out.println("number : " + number[mid]);
                 System.out.println("count : " + count);
                 return number[mid];
             } else {
+                // 찾고자 하는 수가 중앙값보다 더 크면 findIndex의 값을 중앙값에 1 증가후 반복 아니면 1 감소후 반복
                 if(findNum > number[mid]) {
                     System.out.println("찾고자 하는 수 > " + number[mid]);
                     fIndex = mid+1;
@@ -57,6 +74,6 @@ public class BinarySearch {
                 }
             }
         }
-        return -1;
+        return -1; // 수가 없으면 -1 리턴
     }
 }
