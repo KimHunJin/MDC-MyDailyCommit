8467a012c1815e30613fa9d0e59accc9fdd375ca
KimHunJin <ysg01129@naver.com>
2016-09-25 18:48:50 +0900

    FalinuxForm comment

--- Algorithm/src/FalinuxForm.java
+++ Algorithm/src/FalinuxForm.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 /**
  * Created by HunJin on 2016-09-24.
@@ -17,10 +15,11 @@ public class FalinuxForm {
 
     void solve() throws IOException{
 
-        int right = 0, bottom = -1, top = 1;
+        int right = 0, bottom = -1, top = 1; // 시작지점의 오른쪽 값은 0, 바텀값은 -1 (없음), 탑 값음 1
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int size = Integer.parseInt(br.readLine());
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int size = Integer.parseInt(br.readLine());
+        int size = 3;
 
         int[][] falinux = new int[size][size];
 
@@ -29,22 +28,25 @@ public class FalinuxForm {
 
         for(int i=input;i>=0;i--) {
             for(int j=0;j<input;j++) {
-                bottom = bottom + top;
+                bottom = bottom + top;  // top이 0이나 -1로 바뀌면서 바텀값이 증가하거나 감소
                 falinux[right][bottom] = count;
                 count++;
             }
-            input--;
+            input--; // 점점 줄어들면서 반복 진행
             for(int j=0;j<input;j++) {
-                right = right+top;
+                right = right+top;  // top이 스위칭이 되면서 right가 증가하거나 감소
                 falinux[right][bottom] = count;
                 count++;
             }
-            top = top * (-1);
+            top = top * (-1); // 스위칭
         }
-
         print(falinux);
     }
 
+    /**
+     * 출력문
+     * @param faliux
+     */
     void print(int[][] faliux) {
         for (int[] ints : faliux) {
             for (int anInt : ints) {
